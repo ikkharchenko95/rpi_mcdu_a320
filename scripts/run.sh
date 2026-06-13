be4a81dc0f17c4c6a65797b0086e45b6864dc140
Igor Kharchenko <ikkharchenko.95@gmail.com>
2026-06-14 05:54:26 +0700

    [basic_input] Fix run script for make an ability to run project as a linux service

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
 # Determine the root dir of the project
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
-# Go to project root
-cd "$PROJECT_ROOT" || exit 1
+# Go to project cwd
+cd "$PROJECT_DIR" || exit 1
 
-if [ -z "$1" ]; then
-    echo "Error: environment file name not set."
-    echo "Usage: $0 <path_to_env_file>"
-    exit 1
+# Load env from local .env file
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
 fi
 
-ENV_FILE="$1"
-if [ -f "$ENV_FILE" ]; then
-    export $(grep -v '^#' "$ENV_FILE" | xargs)
-else
-    echo "Error: environment file '$ENV_FILE' not found."
-    exit 1
-fi
-
-python3 main.py
+exec python3 main.py "$@"
